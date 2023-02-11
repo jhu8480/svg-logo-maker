036f8f5e1230d705d79c2ec59d1de54d3538606b
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-11 13:49:56 -0500

    added readme.md

--- index.js
+++ index.js
@@ -3,33 +3,7 @@ const inquirer = require('inquirer');
 const Circle = require('./lib/circle');
 const Triangle = require('./lib/triangle');
 const Square = require('./lib/square');
-
-const questions = [
-  {
-    type: 'input',
-    name: 'text',
-    message: 'Please enter text for the logo',
-    validate: function (value) {
-      return value.length <= 3 || 'You can only enter up to three characters';
-    }
-  },
-  {
-    type: 'input',
-    name: 'textColor',
-    message: 'Please enter a color keyword or a hex value for text color',
-  },
-  {
-    type: 'rawlist',
-    name: 'shape',
-    message: 'Please pick a shape',
-    choices: ['circle', 'triangle', 'square']
-  },
-  {
-    type: 'input',
-    name: 'shapeColor',
-    message: 'Please enter the background color (keyword or hex value)'
-  }
-]
+const questions = require('./data/data');
 
 const prompt = inquirer.createPromptModule();
 prompt(questions).then(async ({text, textColor, shape, shapeColor}) => {
@@ -37,17 +11,17 @@ prompt(questions).then(async ({text, textColor, shape, shapeColor}) => {
     const circle = new Circle(text, textColor, shapeColor, shape);
     const svgText = circle.generateSVG();
     await fsPro.writeFile('./circle-logo.svg', svgText);
-    console.log('Circle svg file successfully generated!');
+    console.log('Generated Logo svg!');
   } else if (shape === 'triangle') {
     const triangle = new Triangle(text, textColor, shapeColor, shape);
     const svgText = triangle.generateSVG();
     console.log(svgText);
     await fsPro.writeFile('./triangle-logo.svg', svgText);
-    console.log('Triangle svg file successfully generated!');
+    console.log('Generated Logo svg!');
   } else if (shape === 'square') {
     const square = new Square(text, textColor, shapeColor, shape);
     const svgText = square.generateSVG();
     await fsPro.writeFile('./square-logo.svg', svgText);
-    console.log('Square svg fiile successfully generated');
+    console.log('Generated Logo svg!');
   }
 });
---
+++ readme.md
@@ -0,0 +1,43 @@
+# SVG-Logo-Maker
+
+## Table of Contents
+
+[License](#license)
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[Contributing](#contributing)
+[Questions](#questions)
+
+## License
+
+MIT
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Description
+
+This is a svg logo maker. Users can run the program and generate svg files based on input values such as text color and inner text. There are three shapes available. Color input can be css color keywords or hex values. Examples are in the example folder.
+
+## Installation
+
+run 'npm install' to install dependencies needed for running the program
+
+## Usage
+
+This app is for generating svg logos
+
+## Contributing
+
+N/A
+
+## Questions
+
+Where are my projects stored?
+
+- link to github profile: https://github.com/jhu8480/
+  How do I contact the author?
+- Email: jhu8480@gmail.com
+
+## Keywords
+
+JavaScript node
